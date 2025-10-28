e399ef64d3cd35fa31a9842db184645340572fd5
soolyshine <anna.krasnobaieva@infiz.khpi.edu.ua>
2025-10-28 15:18:35 +0100

    Add files via upload

--- public/lab3/server/main.php
+++ public/lab3/server/main.php
@@ -1,1 +1,16 @@
+<?php 
+$dir = '/cookies/index.html';
 
+if (($_SERVER['REQUEST_METHOD'] ?? '') !== 'POST') {
+    header('Location: ' . $dir);
+    exit;
+
+} else {
+    echo $_SERVER['REMOTE_ADDR'];
+    echo $_SERVER['HTTP_USER_AGENT'];
+    echo $_SERVER['PHP_SELF'];
+    echo $_SERVER['REQUEST_METHOD'];
+    echo $_SERVER['SCRIPT_NAME'];
+}
+
+?>
